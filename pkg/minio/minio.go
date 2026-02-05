d5a578e5ea9ddd81118f81b539870d55332ef75c
Rizal Anggoro <gnoogler4@gmail.com>
2026-02-06 00:28:35 +0700

    Enable SSL configuration for MinIO client based on environment variable

--- pkg/minio/minio.go
+++ pkg/minio/minio.go
@@ -12,10 +12,11 @@ func New() *minio.Client {
 	minioEndpoint := os.Getenv("MINIO_ENDPOINT")
 	minioUser := os.Getenv("MINIO_USER")
 	minioPassword := os.Getenv("MINIO_PASSWORD")
+	minioUseSSL := os.Getenv("MINIO_USE_SSL") == "true"
 
 	client, err := minio.New(minioEndpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(minioUser, minioPassword, ""),
-		Secure: false,
+		Secure: minioUseSSL,
 	})
 	if err != nil {
 		log.Fatalf("failed to connect minio: %v", err)
